319b35478e7e6f761cc3bc3c53e5908e29f13ad6
internsimran2023 <simran.s.brudite@gamil.com>
2023-10-10 16:19:33 +0530

    Initial Commit

---
+++ new.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+def test(name):
+    """ args:
+           name(str): name of entity
+           age(int): age of entity
+           
+           returns:
+           name(str)"""
+           
+    print({name})
+    
+test("simran")
